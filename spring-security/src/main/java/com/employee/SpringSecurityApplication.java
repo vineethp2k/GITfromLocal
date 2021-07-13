2095145338b828f535b73f0498964e9f917b9765
vineethp2k <vineethp2k@gmail.com>
2021-07-13 10:41:02 +0530

    This is 1st commit on eclipse

---
+++ spring-security/src/main/java/com/employee/SpringSecurityApplication.java
@@ -0,0 +1,14 @@
+package com.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApplication {
+
+	public static void main(String[] args) {
+		int a=0;
+		SpringApplication.run(SpringSecurityApplication.class, args);
+	}
+
+}
